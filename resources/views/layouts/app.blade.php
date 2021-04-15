b570b08d4830266ae30396b893bbdcd3f6c4fe2b
Xandy Juanitas <xjuanitas@gmail.com>
2021-04-15 17:39:58 +0800

    fixed dropzone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,4 +18,28 @@
     <script src="{{ asset('assets/dashboard/js/logout.js') }}"></script>
   
     @yield('additional_scripts')
+
+    <script>
+        var previewNode = document.querySelector("#gallery-container");
+        previewNode.id = "";
+        var previewTemplate = previewNode.parentNode.innerHTML;
+        previewNode.parentNode.removeChild(previewNode);
+        
+        $("#dropzoneDragArea").dropzone({
+        url: "null",
+        maxFileSize: 10,
+        uploadMultiple: true,
+        thumbnailWidth: 800,
+        thumbnailHeight: 800,
+        acceptedFiles: "image/*",
+        init: function() {
+            this.on("maxfilesexceeded", function(file){
+            this.removeFile(file);
+            });
+        },
+        previewTemplate: previewTemplate,
+        previewsContainer: "#gallery-preview",
+        clickable: ".gallery",
+        });
+    </script>
 @endpush
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -14,29 +14,4 @@
     <script src="https://rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
     <script src="{{ asset('assets/app/js/app.js') }}"></script>
     <script src="{{ asset('assets/dashboard/js/logout.js') }}"></script>
-
-    <script>
-        var previewNode = document.querySelector("#gallery-container");
-        previewNode.id = "";
-        var previewTemplate = previewNode.parentNode.innerHTML;
-        previewNode.parentNode.removeChild(previewNode);
-        // var $j = jQuery.noConflict();
-
-        $("#dropzoneDragArea").dropzone({
-        url: "null",
-        maxFileSize: 10,
-        uploadMultiple: true,
-        thumbnailWidth: 800,
-        thumbnailHeight: 800,
-        acceptedFiles: "image/*",
-        init: function() {
-            this.on("maxfilesexceeded", function(file){
-            this.removeFile(file);
-            });
-        },
-        previewTemplate: previewTemplate,
-        previewsContainer: "#gallery-preview",
-        clickable: ".gallery",
-        });
-    </script>
 @endpush
