097df55f3a104d69d9350d517d3b2d208c0079e9
Xandy Juanitas <xjuanitas@gmail.com>
2021-04-19 10:01:01 +0800

    added treatment outcome dashboard

---
+++ resources/admin/treatment outcome/index.blade.php
@@ -0,0 +1,149 @@
+<div class="section">
+<div class="section__top">
+    <h1 class="section__title">Treatment outcome</h1>
+    <div class="breadcrumbs"><a class="breadcrumbs__link">Treatment outcome</a><a class="breadcrumbs__link"></a><a class="breadcrumbs__link"></a></div>
+</div>
+<div class="section__container">
+    <a class="button button--create" href="create-treatment.html">Create new case</a>
+    <div class="alert"><span class="alert__text">New case management created</span><button class="button button--transparent js-hide-alert">CLOSE</button></div>
+    <div class="section__content">
+    <ul class="tabs__list tabs__list--table">
+        <li class="tabs__item tabs__item--current">All enrollments</li>
+        <li class="tabs__item">For approval (4)</li>
+        <li class="tabs__item">For follow up (14)</li>
+        <li class="tabs__item">Other suggestion (8)</li>
+        <li class="tabs__item">Not for referral (8)</li>
+    </ul>
+    <div class="tabs__details tabs__details--active">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">Presentation No.</th>
+            <th class="table__head">Facility code</th>
+            <th class="table__head">Patient</th>
+            <th class="table__head">Drug Susceptibility</th>
+            <th class="table__head">Outcome</th>
+            <th class="table__head">Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-treatment.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">Presentation No.</th>
+            <th class="table__head">Facility code</th>
+            <th class="table__head">Patient</th>
+            <th class="table__head">Drug Susceptibility</th>
+            <th class="table__head">Outcome</th>
+            <th class="table__head">Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-enrollment.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">Presentation No.</th>
+            <th class="table__head">Facility code</th>
+            <th class="table__head">Patient</th>
+            <th class="table__head">Drug Susceptibility</th>
+            <th class="table__head">Outcome</th>
+            <th class="table__head">Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-enrollment.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">Presentation No.</th>
+            <th class="table__head">Facility code</th>
+            <th class="table__head">Patient</th>
+            <th class="table__head">Drug Susceptibility</th>
+            <th class="table__head">Outcome</th>
+            <th class="table__head">Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-enrollment.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">Presentation No.</th>
+            <th class="table__head">Facility code</th>
+            <th class="table__head">Patient</th>
+            <th class="table__head">Drug Susceptibility</th>
+            <th class="table__head">Outcome</th>
+            <th class="table__head">Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-enrollment.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    </div>
+</div>
+</div>
