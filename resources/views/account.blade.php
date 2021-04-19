2c46b90ae21cdad8e867a001baa3c63cb8e1615e
Xandy Juanitas <xjuanitas@gmail.com>
2021-04-19 19:47:45 +0800

    added my account page & added sidebar icons

---
+++ resources/views/account.blade.php
@@ -0,0 +1,32 @@
+<div class="section">
+  <div class="section__top">
+    <h1 class="section__title">John Smith</h1>
+    <div class="breadcrumbs"><a class="breadcrumbs__link">My Account</a><a class="breadcrumbs__link"></a><a class="breadcrumbs__link"></a></div>
+  </div>
+  <div class="section__container">
+    <h3 class="section__heading section__heading--unset">John Smith</h3>
+    <p class="form__text">Medical doctor | 5m</p>
+    <br />
+    <br />
+    <form class="form form--quarter">
+      <div class="grid grid--two grid--unset">
+        <div class="form__content"><span class="form__text">john</span><label class="form__label">Username</label></div>
+        <div class="form__content"><span class="form__text">Healthcare</span><label class="form__label">Role </label></div>
+      </div>
+      <hr class="line" />
+      <br />
+      <div class="form__container">
+        <h3 class="section__heading">Main Health Facility</h3>
+        <div class="grid grid--two">
+          <div class="form__content"><span class="form__text">Region 1</span><label class="form__label">Region</label></div>
+          <div class="form__content"><span class="form__text">Smaple</span><label class="form__label">Province </label></div>
+        </div>
+        <div class="grid grid--two">
+          <div class="form__content"><span class="form__text">sample</span><label class="form__label">Municipality</label></div>
+          <div class="form__content"><span class="form__text">Smaple</span><label class="form__label">Facility</label></div>
+        </div>
+      </div>
+      <div class="form__button form__button--start"><button class="button">Logout</button></div>
+    </form>
+  </div>
+</div>
