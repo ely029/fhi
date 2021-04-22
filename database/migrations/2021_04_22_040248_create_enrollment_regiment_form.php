898b98f40b4e4e3936150d9d08247437ac273ce0
ely <eleazarembuscado1@gmail.com>
2021-04-22 08:14:20 +0800

    update migration

--- database/migrations/2021_04_22_040248_create_enrollment_regiment_form.php
+++ database/migrations/2021_04_22_040248_create_enrollment_regiment_form.php
@@ -19,17 +19,23 @@ class CreateEnrollmentRegimentForm extends Migration
                 $table->unsignedBigInteger('form_id');
                 $table->foreign('form_id')->references('id')->on('tb_mac_forms')->onDelete('cascade');
                 
-                $table->longText('treatment_history')->nullable();
+                $table->string('treatment_history')->nullable();
                 $table->string('registration_group')->nullable();
                 $table->string('risk_factor')->nullable();
                 $table->string('drug_susceptibility')->nullable();
                 $table->string('current_weight')->nullable();
                 $table->string('suggested_regimen')->nullable();
                 $table->string('regimen_notes')->nullable();
-                $table->longText('clinical_status')->nullable();
-                $table->longText('signs_and_symptoms')->nullable();
-                $table->longText('vital_signs')->nullable();
-                $table->longText('diag_and_lab_findings')->nullable();
+                $table->string('clinical_status')->nullable();
+                $table->string('signs_and_symptoms')->nullable();
+                $table->string('vital_signs')->nullable();
+                $table->string('diag_and_lab_findings')->nullable();
+                $table->longText('treatment_history')->change();
+                $table->longText('clinical_status')->change();
+                $table->longText('signs_and_symptoms')->change();
+                $table->longText('vital_signs')->change();
+                $table->longText('diag_and_lab_findings')->change();
+                $table->longText('suggested_regimen')->change();
                 $table->timestamps();
             });
         }
