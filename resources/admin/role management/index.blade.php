23eca5d17477db677942cf8f474e46a77954d4cd
Xandy Juanitas <xjuanitas@gmail.com>
2021-04-26 12:37:48 +0800

    integrated admin pages

---
+++ resources/admin/role management/index.blade.php
@@ -0,0 +1,231 @@
+
+<div class="section">
+<div class="section__top">
+    <h1 class="section__title">Role Management</h1>
+    <div class="breadcrumbs"><a class="breadcrumbs__link">Role Management</a><a class="breadcrumbs__link"></a><a class="breadcrumbs__link"></a></div>
+</div>
+<div class="section__container">
+    <a class="button button--create" href="create-admin.html">Create admin</a>
+    <div class="alert"><span class="alert__text">New case management created</span><button class="button button--transparent js-hide-alert">CLOSE</button></div>
+    <div class="section__content">
+    <ul class="tabs__list tabs__list--table">
+        <li class="tabs__item tabs__item--current">All roles</li>
+        <li class="tabs__item">Admin (3)</li>
+        <li class="tabs__item">Healthcare Worker (2)</li>
+        <li class="tabs__item">Regional Secretariat (1)</li>
+        <li class="tabs__item">R-TB MAC (1)</li>
+        <li class="tabs__item">R-TB MAC Chair (1)</li>
+        <li class="tabs__item">N-TB MAC (1)</li>
+        <li class="tabs__item">N-TB MAC Chair (1)</li>
+    </ul>
+    <div class="tabs__details tabs__details--active">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">User ID</th>
+            <th class="table__head">Email Address</th>
+            <th class="table__head">Last Name</th>
+            <th class="table__head">First Name</th>
+            <th class="table__head">Role</th>
+            <th class="table__head">Joining Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-role.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details"></td>
+            <td class="table__details"></td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">User ID</th>
+            <th class="table__head">Email Address</th>
+            <th class="table__head">Last Name</th>
+            <th class="table__head">First Name</th>
+            <th class="table__head">Role</th>
+            <th class="table__head">Joining Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-role.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details"></td>
+            <td class="table__details"></td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">User ID</th>
+            <th class="table__head">Email Address</th>
+            <th class="table__head">Last Name</th>
+            <th class="table__head">First Name</th>
+            <th class="table__head">Role</th>
+            <th class="table__head">Joining Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-role.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details"></td>
+            <td class="table__details"></td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">User ID</th>
+            <th class="table__head">Email Address</th>
+            <th class="table__head">Last Name</th>
+            <th class="table__head">First Name</th>
+            <th class="table__head">Role</th>
+            <th class="table__head">Joining Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-role.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details"></td>
+            <td class="table__details"></td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">User ID</th>
+            <th class="table__head">Email Address</th>
+            <th class="table__head">Last Name</th>
+            <th class="table__head">First Name</th>
+            <th class="table__head">Role</th>
+            <th class="table__head">Joining Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-role.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details"></td>
+            <td class="table__details"></td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">User ID</th>
+            <th class="table__head">Email Address</th>
+            <th class="table__head">Last Name</th>
+            <th class="table__head">First Name</th>
+            <th class="table__head">Role</th>
+            <th class="table__head">Joining Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-role.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details"></td>
+            <td class="table__details"></td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">User ID</th>
+            <th class="table__head">Email Address</th>
+            <th class="table__head">Last Name</th>
+            <th class="table__head">First Name</th>
+            <th class="table__head">Role</th>
+            <th class="table__head">Joining Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-role.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details"></td>
+            <td class="table__details"></td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    <div class="tabs__details">
+        <table class="table table--filter js-table">
+        <thead>
+            <tr>
+            <th class="table__head">User ID</th>
+            <th class="table__head">Email Address</th>
+            <th class="table__head">Last Name</th>
+            <th class="table__head">First Name</th>
+            <th class="table__head">Role</th>
+            <th class="table__head">Joining Date</th>
+            <th class="table__head">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="table__row js-view" data-href="view-role.html">
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details">sample</td>
+            <td class="table__details"></td>
+            <td class="table__details"></td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+    </div>
+</div>
+</div>
