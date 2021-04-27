82116331cae3a34b77782ebdb23e2d7f7851bd8b
Xandy Juanitas <xjuanitas@gmail.com>
2021-04-27 13:19:16 +0800

    integrated email template

---
+++ resources/admin/email.blade.php
@@ -0,0 +1,113 @@
+<div class="email" style="background-color: #ebebeb;
+  padding: 50px 0; box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;">
+    <div class="email__container" style="background-color: #fff;
+  border-top: 4px solid #006168;
+  width: 70%;
+  margin: 0 auto;">
+    <div class="email__logo" style="width: 140px;
+  margin: 50px auto 0;"><img  style="width: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  height: 100%;
+  -webkit-transition: 0.3s;
+  -o-transition: 0.3s;
+  transition: 0.3s;" class="image" src="src/img/logo.png" alt="logo of FHI" /></div>
+    <div class="email__content" style="padding: 50px;">
+        <p class="email__text" style="color: #000;
+  font-size: 20px;
+  margin-top: 30px;margin-top: 0;">Hi [name]</p>
+        <p class="email__text" style="color: #000;
+  font-size: 20px;
+  margin-top: 30px;text-align: justify;">e-TB Mac sent you an email for Admin Account Creation. Please click the button below to create your admin account.</p>
+        <a class="button button--accept" href="" style="width: 30%;
+  margin: 40px auto 70px;
+  height: 50px;
+  font-size: 20px; transition: 0.5s;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  position: relative;
+  font-family: 'Roboto', sans-serif;
+  cursor: pointer;
+  outline: none;
+  background-color: #006168;
+  color: #fff !important;
+  font-size: 1.04167vw;
+  height: 2.70833vw;
+  padding: 0 1.5vw;
+  border: none;
+  text-align: center;">Create account</a>
+        <p class="email__text" style="color: #000;
+  font-size: 20px;
+  margin-top: 30px;display: inline-block;
+  text-align: center;
+  margin: 0 auto;
+  width: 100%;">
+        Need help? Ask our team at<a class="email__link" href="mailto:admin@e-tbmac.com" style="width: 100%;
+  text-align: center;
+  color: #006168;
+  -webkit-transition: 0.4s;
+  -o-transition: 0.4s;
+  transition: 0.4s;
+  margin: 0 5px;">admin@e-tbmac.com</a>
+        <span>
+            or visit our Help Center. <br />
+            This email is auto-generated. Do not reply.
+        </span>
+        </p>
+    </div>
+    </div>
+    <div class="email__footer" style="padding: 80px 290px 0;
+  color: #fff;
+  text-align: center;">
+    <p class="email__text" style="color: #000;
+  font-size: 20px;
+  margin-top: 30px;">© 2020 e-TB MAC. All rights reserved.</p>
+    <ul class="email__footer-list" style="width: 100%;
+  margin: 0 auto;
+  text-align: center;
+  padding: 0;">
+        <li class="email__footer-item" style="display: inline-block;
+  width: 35px;
+  height: 35px;">
+        <a href="" class="email__footer-link" style="width: 100%;
+  height: 100%;"><img class="image" style="width: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  height: 100%;
+  -webkit-transition: 0.3s;
+  -o-transition: 0.3s;
+  transition: 0.3s;" src="src/img/icon-fb.png" alt="facebook icon" /></a>
+        </li>
+        <li class="email__footer-item" style="display: inline-block;
+  width: 35px;
+  height: 35px;">
+        <a href="" class="email__footer-link" style="width: 100%;
+  height: 100%;"><img class="image" style="width: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  height: 100%;
+  -webkit-transition: 0.3s;
+  -o-transition: 0.3s;
+  transition: 0.3s;" src="src/img/icon-ig.png" alt="ig icon" /></a>
+        </li>
+    </ul>
+    <div class="email__logo" style=""><img class="image" src="src/img/logo.png" alt="logo of fhi"  style="width: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  height: 100%;
+  -webkit-transition: 0.3s;
+  -o-transition: 0.3s;
+  transition: 0.3s;"/></div>
+    </div>
+</div>
--- resources/views/enrollments/resubmit/steps/step-3.blade.php
+++ resources/views/enrollments/resubmit/steps/step-3.blade.php
@@ -17,9 +17,9 @@
           <label class="form__label" for="">Drug susceptibility</label>
         </div>
         <div class="form__content form__content--small form-group">
+          <div class="help-block with-errors"></div>
             <input class="form__input" type="number" name="current_weight" value="{{ $tbMacForm->enrollmentForm->current_weight }}" placeholder="Current weight (kg)" required />
             <label class="form__label" for="">Current weight (kg)</label>
-            <div class="help-block with-errors"></div>
         </div>
       </div>
       <div class="form__content">
@@ -42,9 +42,10 @@
         <label class="form__label" for="">Suggested regimen</label>
       </div>
       <div class="form__content itr-drugs">
+      <div class="help-block with-errors"></div>
         <input class="form__input" type="text" id="drugs_given" placeholder="Drugs given" />
         <label class="form__label" for="">Drugs Given</label>
-        <div class="help-block with-errors"></div>
+       
       </div>
       <div class="form__content  other-regimen">
         <div class="help-block with-errors"></div>
@@ -53,32 +54,37 @@
         
       </div>
       <div class="form__content form-group">
+        <div class="help-block with-errors"></div>
           <textarea class="form__input form__input--message" placeholder="" name="regimen_notes" required>{{ $tbMacForm->enrollmentForm->regimen_notes }}</textarea>
           <label class="form__label" for="">Regimen notes</label>
-          <div class="help-block with-errors"></div>
+          
         </div>
     </div>
     <div class="form__container form-step-3">
       <h2 class="section__heading">If for treatment of clinically diagnosed cases</h2>
       <div class="form__content form-group">
+        <div class="help-block with-errors"></div>
           <textarea class="form__input form__input--message" name="clinical_status" placeholder="Clinical Status" required>{{ $tbMacForm->enrollmentForm->clinical_status }}</textarea>
           <label class="form__label" for="">Clinical status</label>
-          <div class="help-block with-errors"></div>
+          
         </div>
       <div class="form__content form-group">
+        <div class="help-block with-errors"></div>
           <input class="form__input" type="text" name="signs_and_symptoms" placeholder="Signs and symptoms" required value="{{ $tbMacForm->enrollmentForm->signs_and_symptoms }}" />
           <label class="form__label" for="">Signs and symptoms</label>
-          <div class="help-block with-errors"></div>
+          
         </div>
       <div class="form__content form-group">
+        <div class="help-block with-errors"></div>
           <input class="form__input" type="text" name="vital_signs" placeholder="Vital signs" required value="{{ $tbMacForm->enrollmentForm->vital_signs }}"/>
           <label class="form__label" for="">Vital signs</label>
-          <div class="help-block with-errors"></div>
+          
         </div>
       <div class="form__content form-group">
+        <div class="help-block with-errors"></div>
           <input class="form__input" type="text" name="diag_and_lab_findings" placeholder="Pertinent diagnostic and laboratory findings" required value="{{ $tbMacForm->enrollmentForm->diag_and_lab_findings }}"/>
           <label class="form__label" for="">Pertinent diagnostic and laboratory findings</label>
-          <div class="help-block with-errors"></div>
+          
         </div>
     </div>
   </div>
