4b5b0d54a9bf3ec528aeda764b66eea98bc1d109
Xandy Juanitas <xjuanitas@gmail.com>
2021-05-10 17:01:53 +0800

    integrated index page of meetings

---
+++ resources/admin/meetings/index.blade.php
@@ -0,0 +1,218 @@
+<div class="section">
+        <div class="section__top">
+          <h1 class="section__title">Meetings</h1>
+          <div class="breadcrumbs"><a class="breadcrumbs__link">Meetings</a><a class="breadcrumbs__link"></a><a class="breadcrumbs__link"></a></div>
+        </div>
+        <div class="section__container">
+          <a class="button button--create" href="create-meeting.html">Create meeting</a>
+          <div class="section__content">
+            <ul class="tabs__list tabs__list--table">
+              <li class="tabs__item tabs__item--current">Meeting</li>
+              <li class="tabs__item">Upcoming</li>
+              <li class="tabs__item">Request</li>
+            </ul>
+            <div class="tabs__details tabs__details--active">
+              <table class="table table--filter js-table">
+                <thead>
+                  <tr>
+                    <th class="table__head">Meeting code</th>
+                    <th class="table__head">Title</th>
+                    <th class="table__head">Date</th>
+                    <th class="table__head">Time</th>
+                    <th class="table__head">Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="table__row js-view" data-href="view-case.html">
+                    <td class="table__details">sample</td>
+                    <td class="table__details">sample</td>
+                    <td class="table__details">sample</td>
+                    <td class="table__details">sample</td>
+                    <td class="table__details">sample</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="tabs__details">
+              <div class="grid grid--meetings">
+                <h2 class="section__heading">May 2021</h2>
+                <hr class="line" />
+              </div>
+              <div class="grid grid--two">
+                <h2 class="section__heading">May 07, 2021</h2>
+                <h2 class="section__heading">May 08, 2021</h2>
+              </div>
+              <ul class="card">
+                <li class="card__item">
+                  <div class="card__content">
+                    <div class="card__top">
+                      <div class="card__top-content">
+                        <span class="form__text form__text--bold">Team Meeting</span>
+                        <span class="form__text form__text--meeting">
+                          samplelink1212 <a class="form__clipboard" href=""> <img class="image image--clipboard" src="src/img/icon-clipboard.png" alt="Clipboard icon" /></a>
+                        </span>
+                      </div>
+                      <img class="image image--flag" src="src/img/icon-calendar.png" alt="calendar icon" />
+                    </div>
+                    <div class="grid grid--two grid--start">
+                      <div class="form__content"><span class="form__text">10:00 AM - 10:45 AM </span><label class="form__label">Time</label></div>
+                      <div class="form__content">
+                        <div class="list">
+                          <span class="list__item">Mary Rose Santiago <span class="list__text">Organizer </span></span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                          <span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                        </div>
+                        <label class="form__label">Guests</label>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="card__item">
+                  <div class="card__content">
+                    <div class="card__top">
+                      <div class="card__top-content">
+                        <span class="form__text form__text--bold">Team Meeting</span>
+                        <span class="form__text form__text--meeting">
+                          samplelink1212 <a class="form__clipboard" href=""> <img class="image image--clipboard" src="src/img/icon-clipboard.png" alt="Clipboard icon" /></a>
+                        </span>
+                      </div>
+                      <img class="image image--flag" src="src/img/icon-calendar.png" alt="calendar icon" />
+                    </div>
+                    <div class="grid grid--two grid--start">
+                      <div class="form__content"><span class="form__text">10:00 AM - 10:45 AM </span><label class="form__label">Time</label></div>
+                      <div class="form__content">
+                        <div class="list">
+                          <span class="list__item">Mary Rose Santiago <span class="list__text">Organizer </span></span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                          <span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                        </div>
+                        <label class="form__label">Guests</label>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+              <div class="grid grid--meetings">
+                <h2 class="section__heading">May 2021</h2>
+                <hr class="line" />
+              </div>
+              <div class="grid grid--two">
+                <h2 class="section__heading">May 07, 2021</h2>
+                <h2 class="section__heading">May 08, 2021</h2>
+              </div>
+              <ul class="card">
+                <li class="card__item">
+                  <div class="card__content">
+                    <div class="card__top">
+                      <div class="card__top-content">
+                        <span class="form__text form__text--bold">Team Meeting</span>
+                        <span class="form__text form__text--meeting">
+                          samplelink1212 <a class="form__clipboard" href=""> <img class="image image--clipboard" src="src/img/icon-clipboard.png" alt="Clipboard icon" /></a>
+                        </span>
+                      </div>
+                      <img class="image image--flag" src="src/img/icon-calendar.png" alt="calendar icon" />
+                    </div>
+                    <div class="grid grid--two grid--start">
+                      <div class="form__content"><span class="form__text">10:00 AM - 10:45 AM </span><label class="form__label">Time</label></div>
+                      <div class="form__content">
+                        <div class="list">
+                          <span class="list__item">Mary Rose Santiago <span class="list__text">Organizer </span></span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                          <span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                        </div>
+                        <label class="form__label">Guests</label>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="card__item">
+                  <div class="card__content">
+                    <div class="card__top">
+                      <div class="card__top-content">
+                        <span class="form__text form__text--bold">Team Meeting</span>
+                        <span class="form__text form__text--meeting">
+                          samplelink1212 <a class="form__clipboard" href=""> <img class="image image--clipboard" src="src/img/icon-clipboard.png" alt="Clipboard icon" /></a>
+                        </span>
+                      </div>
+                      <img class="image image--flag" src="src/img/icon-calendar.png" alt="calendar icon" />
+                    </div>
+                    <div class="grid grid--two grid--start">
+                      <div class="form__content"><span class="form__text">10:00 AM - 10:45 AM </span><label class="form__label">Time</label></div>
+                      <div class="form__content">
+                        <div class="list">
+                          <span class="list__item">Mary Rose Santiago <span class="list__text">Organizer </span></span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                          <span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                        </div>
+                        <label class="form__label">Guests</label>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="tabs__details">
+              <div class="grid grid--meetings">
+                <h2 class="section__heading">May 2021</h2>
+                <hr class="line" />
+              </div>
+              <div class="grid grid--two">
+                <h2 class="section__heading">May 07, 2021</h2>
+                <h2 class="section__heading">May 08, 2021</h2>
+              </div>
+              <ul class="card">
+                <li class="card__item">
+                  <div class="card__content">
+                    <div class="card__top">
+                      <div class="card__top-content">
+                        <span class="form__text form__text--bold">Team Meeting</span>
+                        <span class="form__text form__text--meeting">
+                          samplelink1212 <a class="form__clipboard" href=""> <img class="image image--clipboard" src="src/img/icon-clipboard.png" alt="Clipboard icon" /></a>
+                        </span>
+                      </div>
+                      <img class="image image--flag" src="src/img/icon-calendar.png" alt="calendar icon" />
+                    </div>
+                    <div class="grid grid--two grid--start">
+                      <div class="form__content"><span class="form__text">10:00 AM - 10:45 AM </span><label class="form__label">Time</label></div>
+                      <div class="form__content">
+                        <div class="list">
+                          <span class="list__item">Mary Rose Santiago <span class="list__text">Organizer </span></span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                          <span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                        </div>
+                        <label class="form__label">Guests</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card__bottom">
+                    <span class="form__text">Going?</span>
+                    <div class="card__button"><button class="button button--transparent" type="button">YES</button><button class="button button--transparent" type="button">NO</button></div>
+                  </div>
+                </li>
+                <li class="card__item">
+                  <div class="card__content">
+                    <div class="card__top">
+                      <div class="card__top-content">
+                        <span class="form__text form__text--bold">Team Meeting</span>
+                        <span class="form__text form__text--meeting">
+                          samplelink1212 <a class="form__clipboard" href=""> <img class="image image--clipboard" src="src/img/icon-clipboard.png" alt="Clipboard icon" /></a>
+                        </span>
+                      </div>
+                      <img class="image image--flag" src="src/img/icon-calendar.png" alt="calendar icon" />
+                    </div>
+                    <div class="grid grid--two grid--start">
+                      <div class="form__content"><span class="form__text">10:00 AM - 10:45 AM </span><label class="form__label">Time</label></div>
+                      <div class="form__content">
+                        <div class="list">
+                          <span class="list__item">Mary Rose Santiago <span class="list__text">Organizer </span></span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                          <span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span><span class="list__item">Sample Name</span>
+                        </div>
+                        <label class="form__label">Guests</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card__bottom">
+                    <span class="form__text">Going?</span>
+                    <div class="card__button"><button class="button button--transparent" type="button">YES</button><button class="button button--transparent" type="button">NO</button></div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
