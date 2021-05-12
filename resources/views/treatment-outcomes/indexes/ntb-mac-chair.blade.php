5d5f7a5b4d806707953ba01d222328e155c34acf
ely <eleazarembuscado1@gmail.com>
2021-05-12 16:32:41 +0800

    fix ntbmac chair dashboard

--- resources/views/treatment-outcomes/indexes/ntb-mac-chair.blade.php
+++ resources/views/treatment-outcomes/indexes/ntb-mac-chair.blade.php
@@ -2,17 +2,23 @@
     @include('partials.alerts')
 
     <div class="section__content">
-      <ul class="tabs__list tabs__list--table">
-        <li class="tabs__item tabs__item--current">Referred cases ({{ $referredCases->count() }})</li>
-        <li class="tabs__item">Completed ({{ $completed->count() }})</li>
-        <li class="tabs__item">All cases ({{ $allCases->count() }})</li>
-      </ul>
+    <ul class="tabs__list tabs__list--table">
+    <a href="{{ url('treatment-outcomes?treatmentOutcomeTabs=all_cases') }}">
+            <li class="tabs__item {{ request('treatmentOutcomeTabs') == 'all_cases' ? 'tabs__item--current' : ''}}">All Cases</li>
+        </a>
+        <a href="{{ url('treatment-outcomes?status=Referred to National Chair') }}">
+            <li class="tabs__item {{ request('status') == 'Referred to National chair' ? 'tabs__item--current' : ''}}">Referred Cases</li>
+        </a>
+        <a href="{{ url('treatment-outcomes?status=Referred back to Regional chair') }}">
+            <li class="tabs__item {{ request('status') == 'Referred back to Regional chair' ? 'tabs__item--current' : ''}}">Completed</li>
+        </a>
+    </ul>
       <div class="tabs__details tabs__details--active">
         <table class="table table--filter js-table">
           <thead>
             <tr>
-              <th class="table__head">Presentation No.</th>
-              <th class="table__head">Patient Initials</th>
+              <th class="table__head">Presentation no.</th>
+              <th class="table__head">Patient initials</th>
               <th class="table__head">Age</th>
               <th class="table__head">Sex</th>
               <th class="table__head">Current drug susceptibility</th>
@@ -21,73 +27,17 @@
             </tr>
           </thead>
           <tbody>
-            @foreach($referredCases as $case)
-              <tr class="table__row js-view" data-href="{{ url('case-management/show/'.$case->id.'?from_tab=referred') }}">
-                <td class="table__details">{{ $case->presentation_number }}</td>
-                <td class="table__details">{{ empty($case->patient->initials) ? '' : $case->patient->initials}}</td>
-                <td class="table__details">{{ empty($case->patient->age) ? '' : $case->patient->age}}</td>
-                <td class="table__details">{{ empty($case->patient->gender) ? '' : $case->patient->gender}}</td>
-                <td class="table__details">{{ $case->caseManagementForm->updated_type_of_case ?? ''}}</td>
-                <td class="table__details">{{ $case->created_at->format('m-d-Y')}}</td>
-                <td class="table__details">{{ $case->status }}</td>
-              </tr>
-            @endforeach
-          </tbody>
-        </table>
-      </div>
-      <div class="tabs__details">
-        <table class="table table--filter js-table">
-          <thead>
-            <tr>
-                <th class="table__head">Presentation no.</th>
-                <th class="table__head">Patient initials</th>
-                <th class="table__head">Age</th>
-                <th class="table__head">Sex</th>
-                <th class="table__head">Updated drug susceptibility</th>
-                <th class="table__head">Date submitted by Health Care Worker</th>
-                <th class="table__head">Status</th>
-              </tr>
-          </thead>
-          <tbody>
-            @foreach($completed as $case)
-            <tr class="table__row js-view" data-href="{{ url('case-management/show/'.$case->id) }}">
-              <td class="table__details">{{ $case->presentation_number }}</td>
-              <td class="table__details">{{ empty($case->patient->initials) ? '' : $case->patient->initials}}</td>
-              <td class="table__details">{{ empty($case->patient->age) ? '' : $case->patient->age}}</td>
-              <td class="table__details">{{ empty($case->patient->gender) ? '' : $case->patient->gender}}</td>
-              <td class="table__details">{{ $case->caseManagementForm->updated_type_of_case ?? ''}}</td>
-              <td class="table__details">{{ $case->created_at->format('m-d-Y')}}</td>
-              <td class="table__details">{{ $case->status }}</td>
+          @foreach($cases as $case)
+        <tr class="table__row js-view" data-href="{{url('/treatment-outcomes/'.$case->id.'?from_tab='.request('status'))}}">
+            <td class="table__details">{{ $case->presentation_number }}</td>
+            <td class="table__details">{{ empty($case->patient->initials) ? '' : $case->patient->initials}}</td>
+            <td class="table__details">{{ empty($case->patient->age) ? '' : $case->patient->age}}</td>
+            <td class="table__details">{{ empty($case->patient->gender) ? '' : $case->patient->gender}}</td>
+            <td class="table__details">{{ $case->treatmentOutcomeForm->current_drug_susceptibility }}</td>
+            <td class="table__details">{{ $case->created_at->format('Y-m-d')}}</td>
+            <td class="table__details">{{ $case->status }}</td>
             </tr>
-          @endforeach
-          </tbody>
-        </table>
-      </div>
-      <div class="tabs__details">
-        <table class="table table--filter js-table">
-          <thead>
-            <tr>
-                <th class="table__head">Presentation no.</th>
-                <th class="table__head">Patient initials</th>
-                <th class="table__head">Age</th>
-                <th class="table__head">Sex</th>
-                <th class="table__head">Updated drug susceptibility</th>
-                <th class="table__head">Date submitted by Health Care Worker</th>
-                <th class="table__head">Status</th>
-              </tr>
-          </thead>
-          <tbody>
-            @foreach($allCases as $case)
-            <tr class="table__row js-view" data-href="{{ url('case-management/show/'.$case->id) }}">
-              <td class="table__details">{{ $case->presentation_number }}</td>
-              <td class="table__details">{{ empty($case->patient->initials) ? '' : $case->patient->initials}}</td>
-              <td class="table__details">{{ empty($case->patient->age) ? '' : $case->patient->age}}</td>
-              <td class="table__details">{{ empty($case->patient->gender) ? '' : $case->patient->gender}}</td>
-              <td class="table__details">{{ $case->caseManagementForm->updated_type_of_case ?? ''}}</td>
-              <td class="table__details">{{ $case->created_at->format('m-d-Y')}}</td>
-              <td class="table__details">{{ $case->status }}</td>
-            </tr>
-          @endforeach
+        @endforeach
           </tbody>
         </table>
       </div>
