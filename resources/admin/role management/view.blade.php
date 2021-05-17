96129b7b91f09e66482e3c85319e8e4230fe0bad
Xandy Juanitas <xjuanitas@gmail.com>
2021-05-17 10:24:09 +0800

    role management view pages

--- resources/admin/role management/view.blade.php
+++ resources/admin/role management/view.blade.php
@@ -4,30 +4,21 @@
           <div class="breadcrumbs"><a class="breadcrumbs__link" href="role-management.html">Role management</a><a class="breadcrumbs__link">Juan Dela Cruz</a><a class="breadcrumbs__link"></a></div>
         </div>
         <div class="section__container">
-          <form class="form form--quarter" action="">
+          <form class="form form--half" action="">
             <h2 class="section__heading">Healthcare worker</h2>
             <div class="grid grid--two">
-              <div class="form__content"><span class="form__text">Sample</span><label class="form__label" for="">First name</label></div>
-              <div class="form__content"><span class="form__text">Sample</span><label class="form__label" for="">Last name</label></div>
+              <div class="form__content"><span class="form__text">Sample</span><label class="form__label" for="">Full name</label></div>
+              <div class="form__content"><span class="form__text">1***</span><label class="form__label" for="">Password</label></div>
             </div>
             <div class="grid grid--two">
-              <div class="form__content"><span class="form__text">sample@gmail.com</span><label class="form__label" for="">Email address</label></div>
-              <div class="form__content"><span class="form__text">10/10/10</span><label class="form__label" for="">Joining date</label></div>
+              <div class="form__content"><span class="form__text">ssample</span><label class="form__label" for="">Username</label></div>
+              <div class="form__content"><span class="form__text">Mar 20, 2021</span><label class="form__label" for="">Date created</label></div>
             </div>
-            <div class="form__button form__button--pagination"><button class="button button--accept button--back">Accept</button><button class="button button--decline button--next js-trigger" type="button">Decline</button></div>
-            <div class="modal js-modal">
-              <div class="modal__background js-modal-background"></div>
-              <div class="modal__container">
-                <div class="modal__box">
-                  <h2 class="modal__title">Your message</h2>
-                  <p class="modal__text">By declining this admin, you're require to leave a remarks.</p>
-                  <form class="form">
-                    <div class="form__content"><textarea class="form__input form__input--message" placeholder="Enter remarks"></textarea><label class="form__label" for="">Remarks</label></div>
-                  </form>
-                  <div class="modal__button"><input class="button" type="submit" value="Send message" /></div>
-                </div>
-              </div>
+            <div class="grid grid--two">
+              <div class="form__content"><span class="form__text">ssample@gmail.com</span><label class="form__label" for="">Email address</label></div>
             </div>
+            <div class="form__button form__button--pagination"><button class="button button--accept button--back">Edit</button><button class="button button--decline button--next" type="button">Delete</button></div>
+            
           </form>
         </div>
       </div>
