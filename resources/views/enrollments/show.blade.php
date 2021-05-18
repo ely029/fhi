28daf56e28925574e524d82a4cd6a90cb3166d1f
ely <eleazarembuscado1@gmail.com>
2021-05-18 22:41:45 +0800

    fix empty remarks in enrollment results page

--- resources/views/enrollments/show.blade.php
+++ resources/views/enrollments/show.blade.php
@@ -357,7 +357,7 @@
                   <div class="form__content"><span class="form__text form__text--green">New Enrollment</span><label class="form__label form__label--green">Action</label></div>
                 </div>
                 <span class="form__text">
-                  {{ $tbMacForm->enrollmentForm->remarks }}
+                  {{ $tbMacForm->laboratoryResults->remarks }}
                 </span>
               </div>
             </div>
