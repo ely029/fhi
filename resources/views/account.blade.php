4edaa2a9f583e738139716d2c679c38f4421fd61
Xandy Juanitas <xjuanitas@gmail.com>
2021-05-25 17:41:35 +0800

    integrated kebab menu

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,8 +1,14 @@
 <div class="section">
         <div class="section__top">
+          <div class="section__top-text">
           <h1 class="section__title">John Smith</h1>
           <div class="breadcrumbs"><a class="breadcrumbs__link">My Account</a><a class="breadcrumbs__link"></a><a class="breadcrumbs__link"></a></div>
-        </div>
+          </div>
+          <div class="section__top-menu">
+            <input class="section__top-trigger" type="checkbox" />
+            <div class="section__top-icon"><span> </span><span> </span><span> </span></div>
+            <span class="section__top-popup"><img class="image image--warning" src="src/img/icon-warning.png" alt="warning icon" /><span>Report issue</span></span>
+          </div>
         <div class="section__container">
           <div class="alert"><span class="alert__text">Password successfully changed</span><button class="button button--transparent js-hide-alert">CLOSE</button></div>
           <form class="form form--quarter">
