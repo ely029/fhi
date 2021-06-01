079ef9c8b97a29d95e98568bf2cdb5ba64862e3d
ely <eleazarembuscado1@gmail.com>
2021-06-01 22:29:44 +0800

    fix

--- public/assets/app/js/master-list/remarks.js
+++ public/assets/app/js/master-list/remarks.js
@@ -1,5 +1,6 @@
 jQuery('document').ready(function($){
-    $('#masterlist').find('tr').click(function(){
+    $('#masterlist').on('click', 'tbody tr', function(e){
+        //var e = $(this).index('td.remarks');
         var e = $(this).index();
          var form_id = $('#masterlist tr input#form_id:eq('+ e +')').val();
          var remarks = $('#masterlist tr input#sec_remarks:eq('+ e +')').val();
