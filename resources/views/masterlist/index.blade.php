af6b904add51bee8a07ed25e3d7b4150e1814dce
Xandy Juanitas <xjuanitas@gmail.com>
2021-06-01 13:43:54 +0800

    fixes

--- resources/views/masterlist/index.blade.php
+++ resources/views/masterlist/index.blade.php
@@ -152,7 +152,7 @@
           <div class="modal__box">
             <h2 class="modal__title">Edit remarks</h2>
             <p class="modal__text">Edit remarks for the masterlist.</p>
-            <form class="form" method="POST" action="{{ url('masterlist/update-remarks') }}">
+            <form class="form form--full" method="POST" action="{{ url('masterlist/update-remarks') }}">
             @csrf
               <div class="form__content"><textarea class="form__input form__input--message" name="remarks" placeholder="Enter remarks"></textarea><label class="form__label" for="">Remarks</label></div>
               <div class="modal__button"><input class="button" type="submit" value="Save" /></div>
