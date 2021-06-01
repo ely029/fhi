dc1c017de4b33b16758ae14005a1cba0371b7406
Xandy Juanitas <xjuanitas@gmail.com>
2021-06-01 17:46:55 +0800

    fixes

--- resources/views/case-management/index.blade.php
+++ resources/views/case-management/index.blade.php
@@ -4,9 +4,6 @@
 @section('description', 'Dashboard')
 
 @section('content')
-<div class="wrapper">
-@include('partials.sidebar')
-
 <div class="section">
   <div class="section__top">
     <div class="section__top-text">
@@ -37,21 +34,21 @@
   @if (auth()->user()->role_id === 8)
     @include('case-management.indexes.ntb-mac-chair')
   @endif
-  </div>
+</div>
 
-  <div class="modal" id="remarks" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal__background" data-dismiss="modal"></div>
-    <div class="modal__container">
-        <div class="modal__box">
-            <h2 class="modal__title">Report issue</h2>
-            <p class="modal__text">Please elaborate on the issue encountered.</p>
-            <form class="form form--full" method="POST" action="{{ url('/report-and-feedbacks')}}">
-            @csrf
-                <div class="form__content"> <textarea name="issue" class="form__input form__input--message" placeholder="Enter issue" required></textarea><label class="form__label" for="">Report issue</label></div>
-                <div class="modal__button modal__button--end"><input class="button" type="submit" value="Submit" /></div>
-            </form>
-        </div>
+<div class="modal" id="remarks" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal__background" data-dismiss="modal"></div>
+  <div class="modal__container">
+    <div class="modal__box">
+      <h2 class="modal__title">Report issue</h2>
+      <p class="modal__text">Please elaborate on the issue encountered.</p>
+      <form class="form form--full" method="POST" action="{{ url('/report-and-feedbacks')}}">
+      @csrf
+        <div class="form__content"> <textarea name="issue" class="form__input form__input--message" placeholder="Enter issue" required></textarea><label class="form__label" for="">Report issue</label></div>
+        <div class="modal__button modal__button--end"><input class="button" type="submit" value="Submit" /></div>
+      </form>
     </div>
+  </div>
 </div>
 
 </div>
--- resources/views/enrollments/index.blade.php
+++ resources/views/enrollments/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
-@section('title', 'View Enrollments')
-@section('description', 'View Enrollments')
+@section('title', 'Enrollments')
+@section('description', 'Dashboard')
 
 @section('content')
 
 <div class="section">
-    <div class="section__top">
+  <div class="section__top">
     <div class="section__top-text">
     <h1 class="section__title">Enrollments</h1>
       <div class="breadcrumbs"><a class="breadcrumbs__link">Enrollment regimen</a><a class="breadcrumbs__link"></a><a class="breadcrumbs__link"></a></div>
@@ -36,20 +36,20 @@
     @if (auth()->user()->role_id === 8)
       @include('enrollments.indexes.ntb-mac-chair')
     @endif
-  </div>
-  <div class="modal" id="remarks" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal__background" data-dismiss="modal"></div>
-    <div class="modal__container">
-        <div class="modal__box">
-            <h2 class="modal__title">Report issue</h2>
-            <p class="modal__text">Please elaborate on the issue encountered.</p>
-            <form class="form form--full" method="POST" action="{{ url('/report-and-feedbacks')}}">
-            @csrf
-                <div class="form__content"> <textarea name="issue" class="form__input form__input--message" placeholder="Enter issue" required></textarea><label class="form__label" for="">Report issue</label></div>
-                <div class="modal__button modal__button--end"><input class="button" type="submit" value="Submit" /></div>
-            </form>
-        </div>
+</div>
+<div class="modal" id="remarks" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal__background" data-dismiss="modal"></div>
+  <div class="modal__container">
+    <div class="modal__box">
+      <h2 class="modal__title">Report issue</h2>
+      <p class="modal__text">Please elaborate on the issue encountered.</p>
+      <form class="form form--full" method="POST" action="{{ url('/report-and-feedbacks')}}">
+      @csrf
+        <div class="form__content"> <textarea name="issue" class="form__input form__input--message" placeholder="Enter issue" required></textarea><label class="form__label" for="">Report issue</label></div>
+        <div class="modal__button modal__button--end"><input class="button" type="submit" value="Submit" /></div>
+      </form>
     </div>
+  </div>
 </div>
 @endsection
 @section('additional_scripts')
