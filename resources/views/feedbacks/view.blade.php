341cb7d6f0e480a05b4343a345414cc7436de967
Xandy Juanitas <xjuanitas@gmail.com>
2021-06-03 21:44:55 +0800

    removed report issue in admin

--- resources/views/feedbacks/view.blade.php
+++ resources/views/feedbacks/view.blade.php
@@ -10,11 +10,7 @@
           <h1 class="section__title">Feedback</h1>
           <div class="breadcrumbs"><a class="breadcrumbs__link" href="feedback.html">Feedback</a><a class="breadcrumbs__link">Feedback message</a><a class="breadcrumbs__link"></a></div>
           </div>
-          <div class="section__top-menu">
-            <input class="section__top-trigger" type="checkbox" />
-            <div class="section__top-icon"><span> </span><span> </span><span> </span></div>
-            <span class="section__top-popup"><img class="image image--warning" src="src/img/icon-warning.png" alt="warning icon" /><span>Report issue</span></span>
-          </div>
+          
         </div>
         <div class="section__container">
           <form class="form" action="">
