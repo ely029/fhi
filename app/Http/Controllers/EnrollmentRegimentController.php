ad3d12d5f2c86722955566390ce7d9b22312be30
Lorenz <lorenz@thinkbitsolutions.com>
2021-06-04 10:22:55 +0800

    Debugging:Enrollment recommendation hcw

--- app/Http/Controllers/EnrollmentRegimentController.php
+++ app/Http/Controllers/EnrollmentRegimentController.php
@@ -188,6 +188,7 @@ class EnrollmentRegimentController extends Controller
 
     private function secretariatRecommendation($request)
     {
+        dd($request['status']);
         $tbMacForm = TBMacForm::find($request['form_id']);
         if (in_array($request['status'], ['Not For Referral','Need Further Details'])) {
             $tbMacForm->status = $request['status'];
