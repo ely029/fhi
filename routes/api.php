0e868f17156aed2118148ba1e17f65986d73149d
ely <eleazarembuscado1@gmail.com>
2021-06-08 15:00:50 +0800

    fix pipeline

--- routes/api.php
+++ routes/api.php
@@ -83,8 +83,8 @@ Route::group([
     Route::post('/treatment-outcome/{tbMacForm}/recommendations', [TreatmentOutcomeRecommendationController::class, 'store']);
     Route::get('/treatment-outcome/resubmit/{tbMacForm}', [TreatmentOutcomeResubmitController::class, 'edit']);
     Route::post('/treatment-outcome/resubmit/{tbMacForm}', [TreatmentOutcomeResubmitController::class, 'reSubmit']);
-    Route::get('/masterlist', [MasterListController::class, 'index']);
-    Route::post('/masterlist/filter', [MasterListController::class, 'filter']);
+    Route::get('/masterlist', [MasterlistController::class, 'index']);
+    Route::post('/masterlist/filter', [MasterlistController::class, 'filter']);
     Route::post('masterlist/{id}/update-remarks', [MasterlistController::class, 'updateRemarks']);
     Route::post('/feedbacks', [ReportAndFeedBacksController::class, 'store']);
 });
