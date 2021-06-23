f4c971392aa5cc8e96946a973be94d55bd802ee7
ely <eleazarembuscado1@gmail.com>
2021-06-23 15:34:34 +0800

    remove need further details tab in regional sec and rtbmac chair

--- resources/views/enrollments/indexes/rtb-mac-chair.blade.php
+++ resources/views/enrollments/indexes/rtb-mac-chair.blade.php
@@ -6,7 +6,6 @@
           <li class="tabs__item js-tabs js-tabs-current">Pending ({{ $referred->count() }})</li>
           <li class="tabs__item js-tabs">Completed ({{ $completed->count() }})</li>
           <li class="tabs__item js-tabs">Pending from N-TB MAC Chair ({{ $pendingFromNTBMacChair->count() }})</li>
-          <li class="tabs__item js-tabs">Need Further Details ({{ $needFurtherDetails->count() }})</li>
           <li class="tabs__item js-tabs">All enrollments ({{ $allEnrollments->count() }})</li>
         </ul>
         <div class="tabs__details js-tabs-details js-tabs-details-active">
@@ -92,39 +91,6 @@
             </thead>
             <tbody>
               @foreach($pendingFromNTBMacChair as $enrollment)
-                <tr class="table__row js-view" data-href="{{ url('enrollments/'.$enrollment->id.'?from_tab=pending') }}">
-                  <td class="table__details">{{ $enrollment->presentation_number }}</td>
-                  <td class="table__details">{{ empty($enrollment->patient->facility_code) ? '' : $enrollment->patient->facility_code}}</td>
-                  <td class="table__details">{{ empty($enrollment->patient->province) ? '' : $enrollment->patient->province}}</td>
-                  <td class="table__details">{{ empty($enrollment->patient->initials) ? '' : $enrollment->patient->initials}}</td>
-                  <td class="table__details">{{ empty($enrollment->patient->age) ? '' : $enrollment->patient->age}}</td>
-                  <td class="table__details">{{ empty($enrollment->patient->gender) ? '' : $enrollment->patient->gender}}</td>
- 
-                  <td class="table__details">{{ empty($enrollment->enrollmentForm->drug_susceptibility) ? '' : $enrollment->enrollmentForm->drug_susceptibility}}</td>
-                  <td class="table__details">{{ $enrollment->created_at->format('m-d-Y')}}</td>
-                  <td class="table__details">{{ $enrollment->status }}</td>
-                </tr>
-              @endforeach
-            </tbody>
-          </table>
-        </div>
-        <div class="tabs__details js-tabs-details">
-          <table class="table table--filter js-table">
-            <thead>
-              <tr>
-                <th class="table__head">Presentation no.</th>
-                <th class="table__head">Facility code</th>
-                <th class="table__head">Province</th>
-                <th class="table__head">Patient</th>
-                <th class="table__head">Age</th>
-                <th class="table__head">Sex</th>
-                <th class="table__head">Drug susceptibility</th>
-                <th class="table__head">Date submitted</th>
-                <th class="table__head">Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach($needFurtherDetails as $enrollment)
                 <tr class="table__row js-view" data-href="{{ url('enrollments/'.$enrollment->id.'?from_tab=pending') }}">
                   <td class="table__details">{{ $enrollment->presentation_number }}</td>
                   <td class="table__details">{{ empty($enrollment->patient->facility_code) ? '' : $enrollment->patient->facility_code}}</td>
