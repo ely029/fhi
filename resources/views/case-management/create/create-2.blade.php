56dce908675bd81c87bb8e6659d971e36f396ee9
Xandy Juanitas <xjuanitas@gmail.com>
2021-06-24 02:52:25 +0800

    design revisions

--- resources/views/case-management/create/create-2.blade.php
+++ resources/views/case-management/create/create-2.blade.php
@@ -1,6 +1,155 @@
 <div class="form__tab step-2">
     <h2 class="section__heading">Bacteriological results and status</h2>
     <div class="form__container form-step-2">
+    <table class="table table--unset js-table-unset">
+        <thead>
+        <tr>
+            <th class="table__head no-sort"></th>
+            <th class="table__head">Date collected</th>
+            <th class="table__head">Method used</th>
+            <th class="table__head">Resistance pattern</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="table__row">
+            <td class="table__details">Screening 1</td>
+            <td class="table__details"><input class="form__input form__input--full" type="date" /></td>
+            <td class="table__details">
+            <div class="form__content">
+                <select class="form__input form__input--select form__input--full">
+                <option> </option>
+                <option> </option>
+                </select>
+                <div class="triangle triangle--down"></div>
+                <label class="form__label" for="">Method used </label>
+            </div>
+            </td>
+            <td class="table__details">
+            <div class="form__content">
+                <select class="form__input form__input--select form__input--full">
+                <option> </option>
+                <option> </option>
+                </select>
+                <div class="triangle triangle--down"></div>
+                <label class="form__label" for="">Mycobacterium Tuberculosis (MTB)</label>
+            </div>
+            <div class="form__content">
+                <select class="form__input form__input--select form__input--full">
+                <option> </option>
+                <option> </option>
+                </select>
+                <div class="triangle triangle--down"></div>
+                <label class="form__label" for="">Rifampicin Resistance</label>
+            </div>
+            </td>
+        </tr>
+        <tr class="table__row">
+            <td class="table__details">Screening 2</td>
+            <td class="table__details"><input class="form__input form__input--full" type="date" /></td>
+            <td class="table__details">
+            <div class="form__content">
+                <select class="form__input form__input--select form__input--full">
+                <option> </option>
+                <option> </option>
+                </select>
+                <div class="triangle triangle--down"></div>
+                <label class="form__label" for="">Method used </label>
+            </div>
+            </td>
+            <td class="table__details">
+            <div class="form__content">
+                <select class="form__input form__input--select form__input--full">
+                <option> </option>
+                <option> </option>
+                </select>
+                <div class="triangle triangle--down"></div>
+                <label class="form__label" for="">Mycobacterium Tuberculosis (MTB)</label>
+            </div>
+            <div class="form__content">
+                <select class="form__input form__input--select form__input--full">
+                <option> </option>
+                <option> </option>
+                </select>
+                <div class="triangle triangle--down"></div>
+                <label class="form__label" for="">Rifampicin Resistance</label>
+            </div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+    <div class="form__container">
+                <table class="table table--unset js-table-unset">
+                  <thead>
+                    <tr>
+                      <th class="table__head"></th>
+                      <th class="table__head">Date collected</th>
+                      <th class="table__head">Resistance pattern</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr class="table__row">
+                      <td class="table__details">LPA</td>
+                      <td class="table__details"><input class="form__input form__input--full" type="date" /><span class="with-errors with-errors--table">errow</span></td>
+                      <td class="table__details">
+                        <div class="form__content">
+                          <select class="form__input form__input--select form__input--full">
+                            <option> </option>
+                            <option> </option>
+                          </select>
+                          <div class="triangle triangle--down"></div>
+                          <label class="form__label" for="">Mycobacterium Tuberculosis (MTB)</label>
+                        </div>
+                        <div class="form__content">
+                          <select class="form__input form__input--select form__input--full">
+                            <option> </option>
+                            <option> </option>
+                          </select>
+                          <div class="triangle triangle--down"></div>
+                          <label class="form__label" for="">Isoniazid (h)*</label>
+                        </div>
+                        <div class="form__content">
+                          <select class="form__input form__input--select form__input--full">
+                            <option> </option>
+                            <option> </option>
+                          </select>
+                          <div class="triangle triangle--down"></div>
+                          <label class="form__label" for="">Rifampicin (r)*</label>
+                        </div>
+                        <div class="form__content">
+                          <select class="form__input form__input--select form__input--full">
+                            <option> </option>
+                            <option> </option>
+                          </select>
+                          <div class="triangle triangle--down"></div>
+                          <label class="form__label" for="">Fluoroquinolone (fq)</label>
+                        </div>
+                        <div class="form__content">
+                          <select class="form__input form__input--select form__input--full">
+                            <option> </option>
+                            <option> </option>
+                          </select>
+                          <div class="triangle triangle--down"></div>
+                          <label class="form__label" for="">Second Line Injectable (sli)*</label>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr class="table__row">
+                      <td class="table__details">DST</td>
+                      <td class="table__details"><input class="form__input form__input--full" type="date" /></td>
+                      <td class="table__details">
+                        <div class="form__content">
+                          <select class="form__input form__input--select form__input--full">
+                            <option> </option>
+                            <option> </option>
+                          </select>
+                          <div class="triangle triangle--down"></div>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
     <table class="table table--unset js-table-unset" id="screening-table-1">
         <thead>
         <tr>
